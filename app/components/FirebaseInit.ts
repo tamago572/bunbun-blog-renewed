5e78a805258562fcd5dae627cbf3849791e74f99
tamago572 <110117095+tamago572@users.noreply.github.com>
2025-10-15 23:21:51 +0900

    fix: importを修正

--- app/components/FirebaseInit.ts
+++ app/components/FirebaseInit.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
-import { analytics } from "@/lib/firebaseAnalytics";
+import { analytics } from "@/app/lib/firebaseAnalytics";
 
 export default function FirebaseInit() {
   useEffect(() => {
